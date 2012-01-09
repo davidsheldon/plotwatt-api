b9ba67e7e55901ddb628b6fee7e269dd0bd2a32a
Zach Dwiel <zdwiel@plotwatt.com>
2012-01-09 11:48:14 -0500

    re #393: utcnow -> now

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ class TestDisagDayGeneration(unittest.TestCase):
         self.pw.create_meters(1)
         meter_id = self.pw.list_meters()[0]
         
-        now = datetime.utcnow()
+        now = datetime.now()
         second = seconds = timedelta(seconds=1)
         self.pw.push_readings(meter_id, [1, 2, 3], [now, now + 1*second, now + 2*second])
 
