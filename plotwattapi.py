5b90ef1dd2f76ddcde29a4db1bf164e5b6ac5892
liambuchanan <liam.buchanan@gmail.com>
2015-03-17 12:52:07 -0400

    substitued requests for urllib2

--- plotwattapi.py
+++ plotwattapi.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 """
 Author: Kyle Konrad and Zach Dwiel
 
@@ -7,36 +6,30 @@ This is a basic interface to the plotwatt api.
 Please email comments and questions to zdwiel@plotwatt.com
 """
 
-# urllib2 got renamed to urllib for python3
-# https://stackoverflow.com/questions/6594620/python-3-2-unable-to-import-urllib2-importerror-no-module-named-urllib2
-try:
-    import urllib.request as urllib2
-except:
-    import urllib2
-
+import requests
 import base64
 from datetime import datetime
 import time
-import json
 
-class PlotwattError(Exception) :
-    def __init__(self, message) :
+
+class PlotwattError(Exception):
+    def __init__(self, message):
         self.message = message
-    
-    def __str__(self) :
+
+    def __str__(self):
         return "Plotwatt Error: %s " % (self.message)
 
-class Plotwatt(object) :
-    def __init__(self, house_id, secret, baseurl="http://plotwatt.com") :
+
+class Plotwatt(object):
+    def __init__(self, house_id, secret, baseurl="http://plotwatt.com"):
         self.house_id = house_id
-        self.secret   = secret
-        
+        self.secret = secret
         self.baseurl = baseurl
         self.base64string = base64.encodestring('%s:%s' % (secret, ''))[:-1]
-        self.authheader =  "Basic %s" % self.base64string
-
+        self.authheader = "Basic %s" % self.base64string
+        self.session = requests.Session()
+        self.session.headers.update({"Authorization": self.authheader})
         self.mysql_datetime_format = "'%Y-%m-%d %H:%M:%S'"
-
         # api actions
         self.list_meters_url = '/api/v2/list_meters'
         self.push_readings_url = '/api/v2/push_readings'
@@ -45,19 +38,17 @@ class Plotwatt(object) :
 
     def _request(self, url, data=None):
         """ make a request to plotwatt.com """
-        req = urllib2.Request(self.baseurl + url)
-        req.add_header("Authorization", self.authheader)
-        try :
-            return urllib2.urlopen(req, data, 5)
-        except urllib2.HTTPError as e :
-            if e.code == 422 :
+        try:
+            return self.session.post(self.baseurl + url, data=data)
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 422:
                 raise PlotwattError(e.read())
             raise e
 
     def create_meters(self, num_meters):
         """ create meters on your plotwatt.com account """
         res = self._request(self.new_meters_url, "number_of_new_meters=%s" % num_meters)
-        return json.loads(res.read())
+        return res.json()
 
     def delete_meter(self, meter_id):
         """ delete a meter from your plotwatt.com account """
@@ -66,23 +57,22 @@ class Plotwatt(object) :
     def list_meters(self):
         """ list the meters connected to your plotwatt.com account """
         res = self._request(self.list_meters_url, '')
-        return eval(res.read())
+        return res.json()
 
     def push_readings(self, meter, readings, times):
         """call push_readings endpoint. times should be in POSIX format in UTC"""
-        
         meters = [meter] * len(readings)
-        
-        def sanitize_times(t) :
+
+        def sanitize_times(t):
             """ if the input is a datetime, turn it into a timestamp,
             otherwise, make sure that it is an intiger"""
-            if isinstance(t, datetime) :
+            if isinstance(t, datetime):
                 return str(int(time.mktime(t.timetuple())))
-            else :
+            else:
                 return str(int(t))
-          
+
         times = map(sanitize_times, times)
         data = ','.join(map(lambda tup: "%s,%s,%s" % tup, zip(meters, readings, times)))
         res = self._request(self.push_readings_url, data)
-        assert res.getcode() == 200
+        assert res.status_code == 200
         return res
